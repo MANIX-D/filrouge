a9ca5b93ded3d8fa6193e3198b204c685ef7cd1c
MANIX-D <toyotamnx@gmail.com>
2025-04-16 15:09:48 +0100

    initialisation du projet

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-10 text-red-500">Hello React + Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
